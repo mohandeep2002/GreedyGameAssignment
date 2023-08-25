a5a245ac44a00eab9576cb7bee6b6a743efabf5c
Mohandeep Atmuri <mohandeep2002@gmail.com>
2023-08-26 01:36:59 +0530

    Load, View and Edit JSON Completed || Layout File Saved

--- Assets/Scripts/Editor/JSONViewEditor.cs
+++ Assets/Scripts/Editor/JSONViewEditor.cs
@@ -7,21 +7,22 @@ public class JSONViewEditor : EditorWindow
     private TextAsset templateJson;
     private string loadedJson;
 
-    [MenuItem("GreedyGame/JSON View")]
+    [MenuItem("GreedyGame/Load View Edit JSON")]
     private static void ShowWindow()
     {
-        EditorWindow.GetWindow<JSONViewEditor>("JSON Data", EditorStyles.standardFont);
+        EditorWindow.GetWindow<JSONViewEditor>("Load View Edit JSON", EditorStyles.standardFont);
     }
 
     private void OnGUI()
     {
-        GUILayout.Label("JSON Data", EditorStyles.largeLabel);
-        templateJson = EditorGUILayout.ObjectField("Template JSON", templateJson, typeof(TextAsset), false) as TextAsset;
+        GUILayout.Label("LOAD, VIEW, EDIT JSON Data", EditorStyles.largeLabel);
+        templateJson = EditorGUILayout.ObjectField("JSON File", templateJson, typeof(TextAsset), false) as TextAsset;
         if (GUILayout.Button("Load JSON"))
         {
             if (templateJson != null)
             {
                 loadedJson = templateJson.text;
+                Debug.Log("JSON Data Loaded");
             }
             else
             {
@@ -32,5 +33,33 @@ public class JSONViewEditor : EditorWindow
         GUILayout.Label("Loaded JSON Data: ", EditorStyles.boldLabel);
         loadedJson = EditorGUILayout.TextArea(loadedJson, GUILayout.Height(200));
         GUILayout.Space(10);
+        if (GUILayout.Button("Save Changes"))
+        {
+            if (templateJson != null)
+            {
+                SaveJSONChanges(templateJson, loadedJson);
+            }
+            else
+            {
+                Debug.LogWarning("Check the File");
+            }
+        }
+    }
+
+    private void SaveJSONChanges(TextAsset jsonAsset, string updatedJson)
+    {
+        string oldJson = jsonAsset.text;
+        if (oldJson.Equals(updatedJson))
+        {
+            Debug.LogWarning("Data Not Changed");
+            loadedJson = templateJson.text;
+        }
+        else
+        {
+            string assetPath = AssetDatabase.GetAssetPath(jsonAsset);
+            System.IO.File.WriteAllText(assetPath, updatedJson);
+            AssetDatabase.Refresh();
+            Debug.Log("Data Updated!!");
+        }
     }
 }

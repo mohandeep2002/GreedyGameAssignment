6594500c4377e3644a32d95f3eb6ac441c78f1e2
Mohandeep Atmuri <mohandeep2002@gmail.com>
2023-08-26 00:04:58 +0530

    Worked with UnityEditorWindow

---
+++ Assets/Scripts/Editor/ExampleWindow.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using UnityEditor;
+
+public class ExampleWindow : EditorWindow
+{
+    private Color _color;
+    [MenuItem("Window/Colorizer")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow<ExampleWindow>("Colorizer");
+    }
+    
+    private void OnGUI()
+    {
+        // Window Code
+        GUILayout.Label("Color the selected Objects", EditorStyles.boldLabel);
+        _color = EditorGUILayout.ColorField("Color", _color);
+        if (GUILayout.Button("COLORIZE!"))
+        {
+            Colorize();
+        }
+    }
+
+    private void Colorize()
+    {
+        foreach (var foreSelection in Selection.gameObjects)
+        {
+            Renderer renderer = foreSelection.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.sharedMaterial.color = _color;
+            }
+        }
+    }
+}
